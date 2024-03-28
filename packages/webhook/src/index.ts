6ddc043470fe4d2de740d5bb6dfa86f27c9b3418
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-28 18:04:13 +0900

    add: a initial code for lambda

---
+++ packages/webhook/src/index.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	console.log('event: ', event)
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: 'Hello World' })
+	}
+}
