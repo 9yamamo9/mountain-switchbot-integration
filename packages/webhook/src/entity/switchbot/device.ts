f310abdf60f82f38f0846a970e2c987dc60cda63
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-29 13:48:54 +0900

    add: an initial switchbot entity

---
+++ packages/webhook/src/entity/switchbot/device.ts
@@ -0,0 +1,3 @@
+export class Device {
+	constructor(readonly id: string, readonly status: string, readonly battery: number) {}
+}
--- packages/webhook/src/index.ts
+++ packages/webhook/src/index.ts
@@ -1,7 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { Event } from './type/switchbot/event'
+import { Device } from './entity/switchbot/device'
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-	console.log('event: ', event)
+	const body = JSON.parse(event.body) as Event
+	const device = new Device(body.context.deviceMac, body.context.detectionState, body.context.battery)
 
 	return {
 		statusCode: 200,
