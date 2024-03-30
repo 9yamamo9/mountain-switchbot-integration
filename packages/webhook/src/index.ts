93744c361f72c48b6e18fdc4a96542b931c8ea3c
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-30 13:03:23 +0900

    mod: the handler executes a process

--- packages/webhook/src/index.ts
+++ packages/webhook/src/index.ts
@@ -1,10 +1,25 @@
+import 'reflect-metadata'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { Event } from './type/switchbot/event'
 import Device from './entity/switchbot/device'
+import { container } from 'tsyringe'
+import DeviceDynamoDB from './repogitory/dynamodb/device'
+import DeviceQueue from './repogitory/sqs/device'
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 	const body = JSON.parse(event.body) as Event
+
+	container.register('IDeviceDatabase', {
+		useClass: DeviceDynamoDB
+	})
+
+	container.register('IDeviceQueue', {
+		useClass: DeviceQueue
+	})
+
 	const device = new Device(body.context.deviceMac, body.context.detectionState, body.context.battery)
+
+	await device.notify()
 
 	return {
 		statusCode: 200,
