20e205be7cf2036a04ea21bebb3c6099d72d2e7b
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-04-02 14:43:34 +0900

    fix: specify a resource id in api gateway integration

--- terraform/module/api-gateway/integration.tf
+++ terraform/module/api-gateway/integration.tf
@@ -9,7 +9,7 @@ resource "aws_api_gateway_integration" "webhook" {
 
 resource "aws_api_gateway_integration" "post_remote_controls_turn_off" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.remote_controls_turn_off
+  resource_id             = aws_api_gateway_resource.remote_controls_turn_off.id
   http_method             = aws_api_gateway_method.post_remote_controls_turn_off.http_method
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
