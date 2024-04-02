e57500a1e45f16f8334dcff51564b7a4ef7a8b49
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-04-02 14:41:10 +0900

    fix: specify a resource id in api gateway method

--- terraform/module/api-gateway/method.tf
+++ terraform/module/api-gateway/method.tf
@@ -8,6 +8,6 @@ resource "aws_api_gateway_method" "webhook" {
 resource "aws_api_gateway_method" "post_remote_controls_turn_off" {
   authorization = "NONE"
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.remote_controls_turn_off
+  resource_id   = aws_api_gateway_resource.remote_controls_turn_off.id
   http_method   = "POST"
 }
