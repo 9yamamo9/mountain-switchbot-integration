d103cbe55abcd9e7087d2ce6aedfa36956c74214
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-04-02 16:38:48 +0900

    add: register

--- apps/nature-remo/src/index.ts
+++ apps/nature-remo/src/index.ts
@@ -1,6 +1,13 @@
+import 'reflect-metadata'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import Nature from './entity/nature'
 import { NATURE_APPLIANCE_NICKNAME } from './constant/nature/nature'
+import { container } from 'tsyringe'
+import NatureRemoteControl from './repository/control/nature'
+
+container.register('IRemoteControl', {
+	useClass: NatureRemoteControl
+})
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 	const body = event.body
