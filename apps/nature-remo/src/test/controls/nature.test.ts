63047ef38a2abaa795645d003d6e27679d212fa9
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-04-12 08:59:00 +0900

    add: a test code for an interface to send a message in nature

--- apps/nature-remo/src/test/controls/nature.test.ts
+++ apps/nature-remo/src/test/controls/nature.test.ts
@@ -2,11 +2,16 @@ import 'reflect-metadata'
 import { describe, test } from 'vitest'
 import Nature from '../../entity/nature'
 import { IRemoteControl } from '../../entity/remoteControlInterface'
-import { container, registry } from 'tsyringe'
+import { container } from 'tsyringe'
 
 class FakeRemoteControl implements IRemoteControl {
 	public turnOff = async (): Promise<void> => {}
 }
+
+class FakeChat implements IChat {
+	public send = async (message: string, responseUrl: string): Promise<void> => {}
+}
+
 describe('turnOff', () => {
 	test('success to turn off', async () => {
 		container.register('IRemoteControl', {
